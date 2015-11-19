860332c392e5e9f4c50889afeac55eb175158d78
Tim Claes <tim.claes@me.com>
2015-11-19 15:49:45 +0100

    Added development settings

---
+++ suikerboontje_project/settings_development.py
@@ -0,0 +1,18 @@
+from settings import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+FORCE_SCRIPT_NAME = ''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'claestim_suikerboon',
+        #'NAME': 'claestim_suikerboon_v01',
+        'USER': 'claestim',
+        'PASSWORD': 'LjC7XZgqGtx2',
+        'HOST': '',
+        'PORT': '',
+    }
+}
