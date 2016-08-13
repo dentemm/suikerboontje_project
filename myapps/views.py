16dfe269f5700c10e93bbe09b60f33416f56eb5f
Tim Claes <tim.claes@me.com>
2016-08-13 11:24:50 +0200

    Some code cleanup, and added more responsiveness to presentation page

--- myapps/views.py
+++ myapps/views.py
@@ -23,34 +23,9 @@ class PresentationView(TemplateView):
 
 	def get_context_data(self, **kwargs):
 
-		#images = CustomImage.objects.all()
 		thumbnails = ThumbnailImage.objects.all()
 
-		#image_list = zip(images, thumbnails)
-
-
-		'''myrange = range(1,31)
-
-		urls = []
-
-		for i in myrange:
-
-			if i < 10:
-				url = 'img0' + str(i) +'.jpg'
-
-			else:
-				url = 'img' + str(i) +'.jpg'
-
-
-			urls.append(url)'''
-
 		context = {}
-
-
-
-		#context['images'] = images
-		#context['thumbnails'] = thumbnails
-		#context['image_list'] = image_list
 		context['image_list'] = thumbnails
 
 		return context
